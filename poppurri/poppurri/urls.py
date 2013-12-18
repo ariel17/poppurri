0f86e1eaa21e5662ad9fab12e3f93ce37f6dfae8
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2013-12-18 19:04:11 -0300

    Add basics URL configurations.

--- poppurri/poppurri/urls.py
+++ poppurri/poppurri/urls.py
@@ -8,23 +8,19 @@ __author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
 
 
 from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
+
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='base.html')),
-
-    # Examples:
-    # url(r'^$', 'poppurri.views.home', name='home'),
-    # url(r'^poppurri/', include('poppurri.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
+    # Admin console
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    # Application views
+    url(r'^mixture/', include('mixture.urls')),
+    url(r'/^$', include('web.urls')),
 )
 
 # vim: ai ts=4 sts=4 et sw=4 ft=python
