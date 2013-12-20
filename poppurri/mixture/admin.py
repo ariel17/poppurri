a7f4d8eda59790338617416aae8b0a21a63bd3b6
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2013-12-20 01:15:42 -0300

    Add administration configuration for mixture application.

--- poppurri/mixture/admin.py
+++ poppurri/mixture/admin.py
@@ -9,6 +9,27 @@ __author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Mixture, MixtureImage
+
+
+class MixtureAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'expose')
+    search_fields = (
+        'name',
+        'description',
+        'autor__first_name',
+        'author__last_name',
+        'author__email',
+    )
+    list_filter = ('expose',)
+
+
+class MixtureImageAdmin(admin.ModelAdmin):
+    list_display = ('mixture', 'image')
+    search_fields = ('mixture',)
+
+
+admin.site.register(Mixture, MixtureAdmin)
+admin.site.register(MixtureImage, MixtureImageAdmin)
 
 # vim: ai ts=4 sts=4 et sw=4 ft=python
