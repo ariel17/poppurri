07ebfa8f6832cd260ca5eb110a6456f064bbc5c1
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2013-12-21 14:25:16 -0300

    Change index view name and implementation.

--- poppurri/web/views.py
+++ poppurri/web/views.py
@@ -7,22 +7,24 @@ Description: TODO
 __author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
 
 
-from django.views.generic.list import ListView
+from django.conf import settings
+from django.views.generic.base import TemplateView
 
 from mixture.models import Mixture
 
 
-class HomeListView(ListView):
+class IndexView(TemplateView):
     """
     TODO
     """
-    model = Mixture
     template_name = u"web/index.html"
 
-    # def get_queryset(self):
-    #     """
-    #     TODO
-    #     """
-    #     pass
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+
+        context['top_rates'] = Mixture.objects.\
+            top_rates()[:settings.WEB_CAROUSEL_MIXTURE_COUNT]
+
+        return context
 
 # vim: ai ts=4 sts=4 et sw=4 ft=python
