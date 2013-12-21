27638c4a636ddd88154dd40a0b665f93469e431e
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2013-12-21 14:22:10 -0300

    Add test for models module in mixture app.

---
+++ poppurri/mixture/tests/test_models.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Description: Test cases defined for model/manager classes for application
+             mixture.
+"""
+__author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
+
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+from mixture.models import Mixture
+
+
+class MixtureManagerTestCase(TestCase):
+    """
+    Tests functionallity defined pn :model:`mixture.MixtureManager`.
+    """
+    def setUp(self):
+        """
+        Sets up the environment.
+        """
+        super(MixtureManagerTestCase, self).setUp()
+
+        User = get_user_model()
+        user = User(first_name='test', last_name='test')
+        user.save()
+
+        for score in (1, 5, 5):
+            mixture = Mixture.objects.create(
+                author=user,
+                name='name',
+                description='description'
+            )
+            mixture.rating.add(score=score, user=user, ip_address='127.0.0.1')
+
+    def test_top_rates(self):
+        """
+        Verifies that the number of top rated mixtures matches the instances
+        created.
+        """
+        self.assertEquals(2, Mixture.objects.top_rates().count())
+
+# vim: ai ts=4 sts=4 et sw=4 ft=python
