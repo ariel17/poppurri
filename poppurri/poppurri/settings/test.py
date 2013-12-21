e15005760a14075c1dffd7ff1359b76c730ce429
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2013-12-21 14:24:46 -0300

    Remove deprecated settings.

--- poppurri/poppurri/settings/test.py
+++ poppurri/poppurri/settings/test.py
@@ -10,10 +10,10 @@ __author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
 from base import *
 
 ########## TEST SETTINGS
-TEST_RUNNER = 'discover_runner.DiscoverRunner'
 TEST_DISCOVER_TOP_LEVEL = SITE_ROOT
 TEST_DISCOVER_ROOT = SITE_ROOT
 TEST_DISCOVER_PATTERN = "test_*.py"
+
 ########## IN-MEMORY TEST DATABASE
 DATABASES = {
     "default": {
