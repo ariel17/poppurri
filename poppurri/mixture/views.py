16b34dd67b820f6d06cd870e09d989858657afc6
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2013-12-26 08:47:23 -0300

    Add category tree in context view

--- poppurri/mixture/views.py
+++ poppurri/mixture/views.py
@@ -9,7 +9,7 @@ __author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
 
 from django.views.generic.detail import DetailView
 
-from .models import Mixture
+from .models import Mixture, Category
 
 
 class MixtureDetailView(DetailView):
@@ -19,8 +19,9 @@ class MixtureDetailView(DetailView):
     template_name = u"mixture/detail.html"
     model = Mixture
 
-    # def get_context_data(self, **kwargs):
-    #     context = super(MixtureView, self).get_context_data(**kwargs)
-    #     return context
+    def get_context_data(self, **kwargs):
+        context = super(MixtureDetailView, self).get_context_data(**kwargs)
+        context["category_tree"] = Category.tree(context["object"].category)
+        return context
 
 # vim: ai ts=4 sts=4 et sw=4 ft=python
