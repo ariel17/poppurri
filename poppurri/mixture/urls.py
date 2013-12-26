b321387f9fd2e14de1bc6e7c88cf947d3ad7029f
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2013-12-26 07:49:17 -0300

    Add mixture detail template and view.

--- poppurri/mixture/urls.py
+++ poppurri/mixture/urls.py
@@ -7,10 +7,14 @@ Description: TODO
 __author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
 
 
-from django.conf.urls import patterns
+from django.conf.urls import patterns, url
+
+from .views import MixtureDetailView
 
 
 urlpatterns = patterns('',
+    url(r'^(?P<pk>\d+)/', MixtureDetailView.as_view(), name='mixture_id'),
+    url(r'^(?P<slug>\s+)/', MixtureDetailView.as_view(), name='mixture_slug'),
 )
 
 # vim: ai ts=4 sts=4 et sw=4 ft=python
--- poppurri/mixture/views.py
+++ poppurri/mixture/views.py
@@ -8,7 +8,20 @@ __author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
 
 
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import Mixture
+
+
+class MixtureDetailView(DetailView):
+    """
+    TODO
+    """
+    template_name = u"mixture/detail.html"
+    model = Mixture
+
+    # def get_context_data(self, **kwargs):
+    #     context = super(MixtureView, self).get_context_data(**kwargs)
+    #     return context
 
 # vim: ai ts=4 sts=4 et sw=4 ft=python
