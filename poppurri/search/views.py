517d7147d860e7ae5077cfea30071f25308ed28e
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2013-12-29 15:19:06 -0300

    Add view for search results.

--- poppurri/search/views.py
+++ poppurri/search/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# Create your views here.
+"""
+Description: TODO
+"""
+__author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
+
+
+from django.views.generic.base import TemplateView
+
+
+class SearchView(TemplateView):
+    """
+    TODO
+    """
+    template_name = u"search/result.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(SearchView, self).get_context_data(**kwargs)
+        return context
+
+# vim: ai ts=4 sts=4 et sw=4 ft=python
