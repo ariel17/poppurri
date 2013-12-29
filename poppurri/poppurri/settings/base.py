3ebfd2ebe8c88b7cb23a1d8bc3bfc0f92c78e31f
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2013-12-29 15:17:53 -0300

    Add installed apps to base settings.

--- poppurri/poppurri/settings/base.py
+++ poppurri/poppurri/settings/base.py
@@ -209,7 +209,10 @@ THIRD_PARTY_APPS = (
 
 # Apps specific for this project go here.
 LOCAL_APPS = (
+    'common',
     'mixture',
+    'search',
+    'web',
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
