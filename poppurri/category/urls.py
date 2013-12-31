a30489cef51a4fd89db98df53804b922dd9c6a70
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2013-12-31 00:32:20 -0300

    Change view names.

--- poppurri/category/urls.py
+++ poppurri/category/urls.py
@@ -14,9 +14,10 @@ from .views import CategoryListView, CategoryDetailView
 
 urlpatterns = patterns('',
     url(r'^$', CategoryListView.as_view(), name='category_list'),
-    url(r'^(?P<pk>\d+)/$', CategoryDetailView.as_view(), name='category_id'),
-    url(r'^(?P<slug>[-\w]+)/$', CategoryDetailView.as_view(), \
-        name='category_slug'),
+    url(r'^(?P<pk>\d+)/$', CategoryDetailView.as_view(),
+        name='category_detail_id'),
+    url(r'^(?P<slug>[-\w]+)/$', CategoryDetailView.as_view(),
+        name='category_detail_slug'),
 )
 
 # vim: ai ts=4 sts=4 et sw=4 ft=python
