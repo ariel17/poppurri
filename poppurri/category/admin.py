075fb174d7313d1609ae2df1e8563558e3691a59
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2013-12-31 00:00:46 -0300

    Add initial stage for category app.

---
+++ poppurri/category/admin.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Description: TODO
+"""
+__author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
+
+
+from django.contrib import admin
+
+from .models import Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent', 'is_final')
+    prepopulated_fields = {
+        'slug': ('name', ),
+    }
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+# vim: ai ts=4 sts=4 et sw=4 ft=python
---
+++ poppurri/category/models.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Description: TODO
+"""
+__author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
+
+
+from django.conf import settings
+from django.db import models
+from django.db.models import Count
+from django.utils.translation import ugettext_lazy as _
+
+
+class CategoryManager(models.Manager):
+    """
+    TODO
+    """
+    use_for_related_field = True
+
+    def top_content(self, limit_to=settings.WEB_CATEGORIES_COUNT):
+        """
+        TODO
+        """
+        return self.annotate(num_mixtures=Count('mixtures')).\
+            order_by('-num_mixtures').filter(num_mixtures__gt=0)[:limit_to]
+
+
+class Category(models.Model):
+    """
+    TODO
+    """
+    parent = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        default=None,
+        related_name='children',
+    )
+    name = models.CharField(_(u"Category name"), max_length=100, unique=True)
+    description = models.CharField(
+        _(u"Description"),
+        max_length=255,
+        blank=True,
+        null=True
+    )
+    slug = models.SlugField(max_length=100)
+    is_final = models.BooleanField(_(u"Is a final category"), default=False)
+
+    objects = CategoryManager()
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __unicode__(self):
+        return unicode(self.name)
+
+    def top_mixture(self):
+        """
+        TODO
+        """
+        mixtures = self.mixtures.all()
+        if not len(mixtures):
+            return None
+
+        return mixtures.order_by('-rating_score')[0]
+
+    @classmethod
+    def tree(cls, child_category):
+        """
+        TODO
+        """
+
+        if not child_category:
+            return None
+
+        node = child_category
+        tree = [node]
+
+        while node.parent:
+            tree.append(node.parent)
+            node = node.parent
+
+        tree.reverse()
+        return tree
+
+
+# vim: ai ts=4 sts=4 et sw=4 ft=python
