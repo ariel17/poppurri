b1d1baa9dfcf9d34dd066de901c1314921b65da4
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2014-01-31 19:57:59 -0300

    Add development configurations.

---
+++ poppurri/poppurri/settings/development.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Description: Development settings and globals.
+"""
+__author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
+
+
+from os.path import join, normpath
+
+from base import *
+
+
+########## DEBUG CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
+DEBUG = True
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#template-debug
+TEMPLATE_DEBUG = DEBUG
+########## END DEBUG CONFIGURATION
+
+
+########## EMAIL CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host
+EMAIL_HOST = 'smtp.gmail.com'
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-password
+EMAIL_HOST_PASSWORD = 'password'
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-user
+EMAIL_HOST_USER = 'info@poppurri.com.ar'
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-port
+EMAIL_PORT = 587
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
+EMAIL_SUBJECT_PREFIX = '[%s] ' % SITE_NAME
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-use-tls
+EMAIL_USE_TLS = True
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#server-email
+SERVER_EMAIL = EMAIL_HOST_USER
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email 
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#send-broken-link-emails 
+SEND_BROKEN_LINK_EMAILS = True
+########## END EMAIL CONFIGURATION
+
+
+########## DATABASE CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'poppurri',
+        'USER': 'poppurri',
+        'PASSWORD': 'poppurri',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+########## END DATABASE CONFIGURATION
+
+
+########## CACHE CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#caches
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
+########## END CACHE CONFIGURATION
+
+
+########## TOOLBAR CONFIGURATION
+# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
+
+# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+INTERNAL_IPS = ('127.0.0.1',)
+
+# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+    'SHOW_TEMPLATE_CONTEXT': True,
+}
+########## END TOOLBAR CONFIGURATION
+
+
+########## MIXTURE CONFIGURATION
+MIXTURE_MAX_RATE = 0
+########## END MIXTURE CONFIGURATION
+
+
+########## WEB CONFIGURATION
+WEB_CAROUSEL_MIXTURE_COUNT = 10
+WEB_CATEGORIES_COUNT = 3
+########## END WEB CONFIGURATION
+
+
+########## sorl-thumbnail CONFIGURATION
+THUMBNAIL_DEBUG = DEBUG
+########## END sorl-thumbnail CONFIGURATION
+
+
+########## GOOGLE ANALYTICS CONFIGURATION
+USE_GOOGLE_ANALYTICS = False
+########## END GOOGLE ANALYTICS CONFIGURATION
+
+
+# vim: ai ts=4 sts=4 et sw=4 ft=python
