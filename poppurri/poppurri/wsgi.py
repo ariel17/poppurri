5e55a85e6571c7a0bfe44401b880450e853394b9
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2014-02-01 00:34:27 -0300

    Remove dj_static from wsgi module.

--- poppurri/poppurri/wsgi.py
+++ poppurri/poppurri/wsgi.py
@@ -23,8 +23,6 @@ import os
 from os.path import abspath, dirname
 from sys import path
 
-from dj_static import Cling
-
 
 SITE_ROOT = dirname(dirname(abspath(__file__)))
 path.append(SITE_ROOT)
@@ -39,9 +37,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "poppurri.settings.production")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
