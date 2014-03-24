95a8d8ed14e7d7562edd4b82d82efccb313cbdd9
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2014-03-24 00:43:04 -0300

    Change tests to match actual state.

--- poppurri/currency/tests/test_templatetags.py
+++ poppurri/currency/tests/test_templatetags.py
@@ -22,19 +22,16 @@ class ToCurrencyTestCase(TestCase):
     """
     def setUp(self):
         super(ToCurrencyTestCase, self).setUp()
-        Currency.objects.create(code='ARS', symbol='$', value='10')
-        Currency.objects.create(code='USD', symbol='$', value='1')
 
     def tearDown(self):
         super(ToCurrencyTestCase, self).tearDown()
-        Currency.objects.all().delete()
 
     def test_to_currency(self):
         """
         Tests a successful currency convertion, when the scale exists in model.
         """
         currency = Currency.objects.get(code='ARS')
-        self.assertEquals(Decimal('16.00'),
+        self.assertEquals(Decimal('13'),
                           to_currency(Decimal('1.55'), currency))
 
         currency = Currency.objects.get(code='USD')
