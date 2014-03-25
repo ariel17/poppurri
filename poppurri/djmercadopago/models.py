18a9820830e56a566cf6d509dc81bef3eddb308e
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2014-03-25 01:51:28 -0300

    Add payment model definitions.

--- poppurri/djmercadopago/models.py
+++ poppurri/djmercadopago/models.py
@@ -3,22 +3,225 @@
 
 """
 Description: Payment models for MercadoPago API.
+See: https://developers.mercadopago.com/
 """
 __author__ = "Ariel Gerardo Rios (ariel.gerardo.rios@gmail.com)"
 
 
-from django.db import models
+import json
+import logging
+import uuid
+
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+import mercadopago
 
 
-REQUIRED_SETTINGS = ['MERCADOPAGO_ACCESS_TOKEN',]
+REQUIRED_SETTINGS = [
+    'MERCADOPAGO_CLIENT_ID',
+    'MERCADOPAGO_CLIENT_SECRET',
+    'MERCADOPAGO_ACCESS_TOKEN',
+]
 
 for key in REQUIRED_SETTINGS:
     if not getattr(settings, key):
         raise ImproperlyConfigured('Missing required setting: %s' % key)
 
+CLIENT_ID = settings.MERCADOPAGO_CLIENT_ID
+
+CLIENT_SECRET = settings.MERCADO_CLIENT_SECRET
+
 ACCESS_TOKEN = settings.MERCADOPAGO_ACCESS_TOKEN
 
+USE_SANDBOX = getattr(settings, 'MERCADOPAGO_USE_SANDBOX', False)
 
-class 
+_CLIENT = mercadopago.MP(CLIENT_ID, CLIENT_SECRET)
+
+LOGGER = logging.getLogger(__name__)
+
+
+class PaymentType(models.Model):
+    """
+    Payment type or category.
+    """
+    name = models.CharField(_('Name'), )
+    mercadopago_id = models.CharField(_('MercadoPago id'), )
+
+
+class PaymentMethod(models.Model):
+    """
+    The payment description.
+    """
+    name = models.CharField(_('Name'), )
+    mercadopago_id = models.CharField(_('MercadoPago id'), )
+    payment_type = models.ForeignKey(PaymentType)
+    thumbnail = models.URLField(_('Thumbnail URL'), )
+    secure_thumbnail = models.URLField(_('Secure thumbnail URL'), )
+
+
+class PaymentPreferenceManager(models.Manager):
+    """
+    Overwrites the create method in order to use the MercadoPago API client.
+    """
+    def create(self, preference, *args, **kwargs):
+        """
+        Creates a :model:`djmercadopago.PaymentPreference` instance using the
+        MercadoPago API client and stores the response infomation.
+        """
+        _uuid = uuid.uuid4()
+
+        LOGGER.debug('[%s] Requesting creation of payment preferences: %r '
+                     '...' % (_uuid, preference, ))
+
+        response = json.dumps(_CLIENT.create_preference(preference), indent=4)
+
+        LOGGER.debug('[%s] Done. Response: %s' % (_uuid, response))
+
+        params = {
+            # TODO add parameters from response to create preference object
+        }
+
+        kwargs.update(params)
+        return super(PaymentPreferenceManager, self).create(*args, **kwargs)
+
+
+class PaymentPreference(models.Model):
+    """
+    Customizes the payment preferences to use.
+    See: http://developers.mercadopago.com/documentacion/api/preferences
+    """
+    items_title = models.CharField(
+        _('Title'),
+        help_text=_('Mandatory; it will be shown on payment process.'),
+    )
+    items_quantity = models.PositivieIntegerField(
+        _('Quantity'),
+        help_text=_('Mandatory.'),
+    )
+    items_unit_price = models.DecimalField(
+        _('Unit price'),
+        help_text=_('Mandatory.'),
+    )
+    items_currency_id = models.CharField(
+        _('Currency type'),
+        help_text=_(
+            'mandatory. Argentina (argentinian peso): ARS, Brazil (Real): '
+            'brl, Mexico (mexican peso): MXN, Venezuela (Strong Bolivar): '
+            'vef, Colombia (colombian peso): COP'
+        ),
+    )
+    items_picture_url = models.URLField(
+        _('Image URL'),
+        blank=True,
+        null=True,
+        default=None,
+        help_text=_('Optional.'),
+    )
+    items_id = models.CharField(
+        _('Code'),
+        blank=True,
+        null=True,
+        default=None,
+        help_text=_('Optional.'),
+    )
+    items_description = models.CharField(
+        _('description'),
+        blank=True,
+        null=True,
+        default=None,
+        help_text=_('Optional.'),
+    )
+
+    payer_name = models.CharField(
+        _('Buyer name'),
+        blank=True,
+        null=True,
+        default=None,
+        help_text=_('Optional.'),
+    )
+    payer_surename = models.CharField(
+        _('Buyer surename'),
+        blank=True,
+        null=True,
+        default=None,
+        help_text=_('Optional.'),
+    )
+    payer_email = models.EmailField(
+        _('Buyer email'),
+        blank=True,
+        null=True,
+        default=None,
+        help_text=_('Optional.'),
+    )
+
+    back_urls_success = models.URLField(
+        _('Success payment URL'),
+        blank=True,
+        null=True,
+        default=None,
+        help_text=_('Optional.'),
+    )
+    back_urls_failure = models.URLField(
+        _('Failed payment URL'),
+        blank=True,
+        null=True,
+        default=None,
+        help_text=_('Optional.'),
+    )
+    back_urls_pending = models.URLField(
+        _('Pending payment URL'),
+        blank=True,
+        null=True,
+        default=None,
+        help_text=_('Optional.'),
+    )
+
+    payment_methods_excluded_payment_methods = models.ManyToMany(
+        _('Excluded payment methods'),
+        PaymentMethod,
+        null=True,
+        default=None,
+    )
+    payment_methods_excluded_payment_types = models.ManyToMany(
+        _('Excluded payment types'),
+        PaymentType,
+        null=True,
+        default=None,
+    )
+    payment_methods_installments = models.PositiveIntegerField(
+        _('Maximum installments'),
+        null=True,
+        default=None,
+        help_text=_('Optional. Maximum number of installments you will to '
+                    'accept with credit card.'),
+    )
+
+    external_reference = models.CharField(
+        _('External reference'),
+        blank=True,
+        null=True,
+        help_text=_('Optional. Your system ID to link it to the payment.'),
+    )
+    collector_id = models.CharField(
+        _('Collector ID'),
+        help_text=_('Your identification ID as seller.'),
+    )
+    init_point = models.URLField(
+        _('Init point'),
+        help_text=_('Checkout access point.'),
+    )
+    sandbox_init_point = models.URLField(
+        _('Sandbox init point'),
+        help_text=_('Checkout access point on sandbox environment.'),
+    )
+    date_created = models.DateTimeField(
+        _('Date created'),
+    )
+
+    objects = PaymentPreferenceManager()
+
+
+# vim: ai ts=4 sts=4 et sw=4 ft=python
